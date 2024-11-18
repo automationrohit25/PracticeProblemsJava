77f2cb7132bc1f24e236a3050edc9c799c60fc7c
automationrohit25 <67341642+automationrohit25@users.noreply.github.com>
2024-11-19 00:35:54 +0530

    second commit

---
+++ src/interviewprobssdetqa/ReverseArrayOnGroup.java
@@ -0,0 +1,13 @@
+package interviewprobssdetqa;
+
+public class ReverseArrayOnGroup {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] inputArr = {1,2,3,4,5,6,7}; 
+//		output = {3,2,1,6,5,4,7}
+
+	}
+
+}
